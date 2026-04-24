dc89671d317bed9a0d2fd31f5c167d5382b62230
Даниил <pochtanew31@gmail.com>
2026-04-24 17:54:14 +0700

    origins

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(title="FIPI Bank Solve Platform")
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"], # Adjust for your frontend URL
+    allow_origins=["*"], # Adjust for your frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
